418656c6f22864233c71e5759120aa7c6e03147c
Sung M. Kim <sungmkim80@gmail.com>
2019-08-10 19:53:33 -0400

    Added a credit to react-dropzone
    For providing own refs

--- src/useSticky.tsx
+++ src/useSticky.tsx
@@ -21,6 +21,8 @@ const sentinelClasses = {
 function useSticky<StickyProps>() {
   // Let users assign refs instead of letting them create their own~
   // Easier to use & less boiler-plate to write.
+  // Got the idea from react-dropzone
+  // https://github.com/react-dropzone/react-dropzone/blob/acea5df30f272f3071570c74a07730d1a8f99120/src/index.js#L391
   const containerRef = React.useRef(document.body);
   const stickyRef = React.useRef(document.body.firstElementChild);
 
